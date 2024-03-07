f9d135424a102ffc4ef2fa745ddb3653042b2172
David <davidmcginley1@hotmail.com>
2024-03-07 14:44:59 +0000

    Add constructor and getters/setters to Ingredient.java

--- Ingredient.java
+++ Ingredient.java
@@ -1,8 +1,9 @@
 public class Ingredient {
     private String id;
     private String name;
-    private String type;
+    private String type;    // e.g. meat, fish, veg, fruit, grain, dairy?
     private int calories;
+    // Could include other nutritional info:
     /*
     private int protein;
     private int fat;
@@ -11,12 +12,42 @@ public class Ingredient {
     private int salt;
     */
 
-    /*
-    Get Name
-    Get type
-    IsVegan
-    IsVegetarian
-    IsAllergen
-    IsDairy
-     */
+    public Ingredient(String id, String name, String type, int calories) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.calories = calories;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
 }
