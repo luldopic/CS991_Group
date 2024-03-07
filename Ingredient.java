cb2d29d39376e10ec83edf468c01dc0a4733b402
luldopic <27344798+luldopic@users.noreply.github.com>
2024-03-07 14:08:47 +0000

    Updating attributes for Ingredient class

--- Ingredient.java
+++ Ingredient.java
@@ -1,2 +1,10 @@
 public class Ingredient {
+    private String id;
+    private String name;
+    private String type;
+    private int calories;
+    private int protein;
+    private int fat;
+    private int carbs;
+    private int sugar;
 }
