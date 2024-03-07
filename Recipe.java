fcb6374f7ceeb4ce4bbeeba5518029d981dc2e17
luldopic <27344798+luldopic@users.noreply.github.com>
2024-03-07 13:54:40 +0000

    Updating attributes for Recipe class

--- Recipe.java
+++ Recipe.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Date;
 public class Recipe {
+    String id;
+    String name;
+    HashMap<Ingredient, Integer> ingredientList;
+    String instruction;
+    int cookTime;
+
 }
