3ae8a5ddfc0ea8f6032f66ef27bae85a61e39ab3
David <davidmcginley1@hotmail.com>
2024-03-07 14:52:19 +0000

    Add constructors and getters/setters to Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -7,11 +7,76 @@ public class Recipe {
     private int cookTime;
     private int prepTime;
 
-    public Recipe(){
-
+    public Recipe(String name, HashMap<Ingredient, Integer> ingredientList,
+                  String instruction, int cookTime, int prepTime)
+    {
+        id = generateId();
+        this.name = name;
+        this.ingredientList = ingredientList;
+        this.instruction = instruction;
+        this.cookTime = cookTime;
+        this.prepTime = prepTime;
     }
 
-    //Method
+    public Recipe (String name)
+    {
+        id = generateId();
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String generateId() {
+        return "0";
+        // placeholder
+        // TODO refactor to randomly generate ID instead
+        // real id will be based on user id plus time so should be unique
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HashMap<Ingredient, Integer> getIngredientList() {
+        return ingredientList;
+    }
+
+    public void setIngredientList(HashMap<Ingredient, Integer> ingredientList) {
+        this.ingredientList = ingredientList;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public int getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(int cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public int getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(int prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    // Methods
+
     /*
     Add ingredients
     Edit ingredients
